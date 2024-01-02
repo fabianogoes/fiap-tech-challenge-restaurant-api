86d781db75b95d1e2f582a29b373f07735de48fb
Fabiano Góes <fabianogoes@gmail.com>
2024-01-02 20:56:21 -0300

    implemented customer port repository and usecase

--- internal/core/domain/customer.go
+++ internal/core/domain/customer.go
@@ -3,10 +3,18 @@ package domain
 import "time"
 
 type Customer struct {
-	ID        int64
+	ID        uint
 	Nome      string
 	Email     string
 	CPF       string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func NewCustomer(nome string, email string, cpf string) (*Customer, error) {
+	return &Customer{
+		Nome:  nome,
+		Email: email,
+		CPF:   cpf,
+	}, nil
 }
--- internal/core/port/customer.go
+++ internal/core/port/customer.go
@@ -5,19 +5,20 @@ import "github.com/fiap/challenge-gofood/internal/core/domain"
 // Primary ports to Customer
 
 type CustomerUseCasePort interface {
-	CreateCustomer(product *domain.Customer) (*domain.Customer, error)
-	GetCustomer(id int64) (*domain.Customer, error)
+	CreateCustomer(nome string, email string, cpf string) (*domain.Customer, error)
+	GetCustomerById(id uint) (*domain.Customer, error)
 	GetCustomers() ([]*domain.Customer, error)
-	UpdateCustomer(product *domain.Customer) (*domain.Customer, error)
-	DeleteCustomer(id int64) error
+	UpdateCustomer(customer *domain.Customer) (*domain.Customer, error)
+	DeleteCustomer(id uint) error
 }
 
 // Secondary ports to Customer
 
 type CustomerRepositoryPort interface {
-	CreateCustomer(product *domain.Customer) (*domain.Customer, error)
-	GetCustomer(id int64) (*domain.Customer, error)
+	CreateCustomer(customer *domain.Customer) (*domain.Customer, error)
+	GetCustomerByCPF(cpf string) (*domain.Customer, error)
+	GetCustomerById(id uint) (*domain.Customer, error)
 	GetCustomers() ([]*domain.Customer, error)
-	UpdateCustomer(product *domain.Customer) (*domain.Customer, error)
-	DeleteCustomer(id int64) error
+	UpdateCustomer(customer *domain.Customer) (*domain.Customer, error)
+	DeleteCustomer(id uint) error
 }
