f413079a5bf75e9b8d14a87ce0b30a27ca228d76
Fabiano Góes <fabianogoes@gmail.com>
2024-01-05 17:33:24 -0300

    add script sql to clean database

---
+++ scripts/clean-data.sql
@@ -0,0 +1,8 @@
+DROP TABLE "payments";
+DROP TABLE "deliveries";
+DROP TABLE "order_items";
+DROP TABLE "orders";
+DROP TABLE "attendants";
+DROP TABLE "customers";
+DROP TABLE "products";
+DROP TABLE "categories";
