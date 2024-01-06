12ccbd6d7d750ecf87a7c78398c16e64824813d9
Fabiano Góes <fabianogoes@gmail.com>
2024-01-06 19:40:16 -0300

    refactor payment domain

--- internal/core/domain/payment.go
+++ internal/core/domain/payment.go
@@ -3,7 +3,6 @@ package domain
 import "time"
 
 type PaymentStatus int
-type PaymentMethod int
 
 const (
 	PaymentStatusPending PaymentStatus = iota
@@ -11,11 +10,47 @@ const (
 	PaymentStatusCanceled
 )
 
+func (ps PaymentStatus) ToString() string {
+	return [...]string{"PENDING", "PAID", "CANCELED"}[ps]
+}
+
+func (ps PaymentStatus) ToPaymentStatus(status string) PaymentStatus {
+	switch status {
+	case "PENDING":
+		return PaymentStatusPending
+	case "PAID":
+		return PaymentStatusPaid
+	case "CANCELED":
+		return PaymentStatusCanceled
+	default:
+		return PaymentStatusPending
+	}
+}
+
+type PaymentMethod int
+
 const (
 	PaymentMethodCreditCard PaymentMethod = iota
 	PaymentMethodDebitCard
 	PaymentMethodMoney
 )
+
+func (pm PaymentMethod) ToString() string {
+	return [...]string{"CREDIT_CARD", "DEBIT_CARD", "MONEY"}[pm]
+}
+
+func (pm PaymentMethod) ToPaymentMethod(method string) PaymentMethod {
+	switch method {
+	case "CREDIT_CARD":
+		return PaymentMethodCreditCard
+	case "DEBIT_CARD":
+		return PaymentMethodDebitCard
+	case "MONEY":
+		return PaymentMethodMoney
+	default:
+		return PaymentMethodMoney
+	}
+}
 
 type Payment struct {
 	ID        int64
