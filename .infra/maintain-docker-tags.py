3e341df41139ea66132c617df5f3785066dc87b7
Fabiano Góes <fabianogoes@gmail.com>
2024-05-01 18:25:18 -0300

    fix python

--- .infra/maintain-docker-tags.py
+++ .infra/maintain-docker-tags.py
@@ -1,26 +1,26 @@
 import docker
-import os
 from datetime import datetime
+import os  # Importe a biblioteca os para acessar variáveis de ambiente
 
 # Nome do repositório no Docker Hub
-repository_name = "fabianogoes/restaurant-api"
+repository_name = "sua-conta/seu-repositorio"
 
 # Número máximo de tags a serem mantidas
-max_tags = 3
+max_tags = 5
 
 # Autenticação no Docker Hub
-client = docker.DockerClient(base_url='unix://var/run/docker.sock')  # Use a API local do Docker
+client = docker.APIClient(base_url='unix://var/run/docker.sock')  # Use a API local do Docker
 client.login(username=os.environ['DOCKER_USERNAME'], password=os.environ['DOCKER_PASSWORD'])
 
 # Obter lista de tags do repositório
-tags = client.api.tags(repository_name)
+tags = client.list_tags(repository_name)
 
 # Ordenar as tags por data
 tags.sort(key=lambda x: datetime.strptime(x['last_updated'], "%Y-%m-%dT%H:%M:%S.%fZ"), reverse=True)
 
 # Excluir tags extras
 for tag in tags[max_tags:]:
-    client.api.delete(f"/v2/repositories/{repository_name}/tags/{tag['name']}")
+    client.delete(f"/v2/repositories/{repository_name}/tags/{tag['name']}")
 
 print(f"As seguintes tags foram mantidas em {repository_name}:")
 for tag in tags[:max_tags]:
