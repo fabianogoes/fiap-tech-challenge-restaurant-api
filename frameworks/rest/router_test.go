1e800075b8f89fed3fad7ef3ac94ab1d79e3c85c
Fabiano Góes <fabianogoes@gmail.com>
2024-06-29 02:22:48 -0300

    product test rest

---
+++ frameworks/rest/router_test.go
@@ -0,0 +1,44 @@
+package rest
+
+import (
+	"github.com/fabianogoes/fiap-challenge/domain"
+	"github.com/fabianogoes/fiap-challenge/domain/entities"
+	"github.com/fabianogoes/fiap-challenge/domain/usecases"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Router(t *testing.T) {
+	config, _ := entities.NewConfig()
+	customerRepository := new(domain.CustomerRepositoryMock)
+	customerHandler := NewCustomerHandler(usecases.NewCustomerService(customerRepository), config)
+	attendantRepository := new(domain.AttendantRepositoryMock)
+	attendantHandler := NewAttendantHandler(usecases.NewAttendantService(attendantRepository))
+	productRepository := new(domain.ProductRepositoryMock)
+	productHandler := NewProductHandler(usecases.NewProductService(productRepository))
+	orderService := usecases.NewOrderService(
+		new(domain.OrderRepositoryMock),
+		customerRepository,
+		attendantRepository,
+		usecases.NewPaymentService(new(domain.PaymentRepositoryMock)),
+		new(domain.PaymentClientMock),
+		new(domain.DeliveryClientMock),
+		new(domain.DeliveryRepositoryMock),
+		new(domain.KitchenClientMock),
+	)
+	orderHandler := NewOrderHandler(
+		orderService,
+		usecases.NewCustomerService(customerRepository),
+		usecases.NewAttendantService(attendantRepository),
+		usecases.NewProductService(productRepository),
+	)
+
+	router, err := NewRouter(
+		customerHandler,
+		attendantHandler,
+		productHandler,
+		orderHandler,
+	)
+	assert.NoError(t, err)
+	assert.NotNil(t, router)
+}
